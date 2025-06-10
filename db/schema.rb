afb59e461137b01a36bea1139184d40ebc7514a0
ollie dwyer <olliedwyer@gmail.com>
2025-06-10 21:37:03 +1000

    card

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_10_101540) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
