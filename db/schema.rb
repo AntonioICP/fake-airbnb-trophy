1b4d4d2d1d9c4f8338fcbe26db4de0768f9ae525
antonio cisneros <acip0903@gmail.com>
2025-06-14 16:30:23 +1000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_14_042702) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
