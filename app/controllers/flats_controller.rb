56460bec54843648aab1915bda10b47c87fd13a7
ollie dwyer <olliedwyer@gmail.com>
2025-06-14 15:17:36 +1000

    added map, still need to move the map into position.

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,10 +2,22 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
     @request = Request.new
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
     @flat = Flat.find(params[:id])
     @request = Request.new
+    @markers = [
+      {
+        lat: @flat.latitude,
+        lng: @flat.longitude
+      }
+    ]
   end
 end
