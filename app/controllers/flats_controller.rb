a599360497b05abc3a27842ecfea52f65712172f
MariaVega <mariavegag2@gmail.com>
2025-06-17 19:52:23 +1000

    search location

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,7 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
     if params[:query].present?
-      @flats = @flats.where("name ILIKE ?", "%#{params[:query]}%")
+      @flats = @flats.where("address ILIKE ?", "%#{params[:query]}%")
     end
     @markers = @flats.geocoded.map do |flat|
       {
